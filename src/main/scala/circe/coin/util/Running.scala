8d720f5c2848add0d58c29c4c19aaa5962016e8e
Phuong Lam <phuong@circe.xyz>
2018-03-09 17:21:52 +0700

    Add common exception mapper

---
+++ src/main/scala/circe/coin/util/Running.scala
@@ -0,0 +1,30 @@
+package lbt.util
+
+import java.util.{Timer, TimerTask}
+
+/**
+ * Created by plt on 12/6/17.
+ **/
+
+trait Running extends XLogging {
+
+  private val globalTimer = new Timer()
+
+  def run(delay: Long, period: Long)(func: => Unit): Unit = {
+    globalTimer.schedule(new TimerTask {
+      override def run(): Unit = try {
+        func
+      } catch {
+        case throwable: Throwable => exception(throwable, "Running.run")
+      }
+    }, delay, period)
+  }
+
+  def runWithNewTimer(delay: Long, period: Long)(func: => Unit): Timer = {
+    val timer = new Timer()
+    timer.schedule(new TimerTask {
+      override def run(): Unit = func
+    }, delay, period)
+    timer
+  }
+}
