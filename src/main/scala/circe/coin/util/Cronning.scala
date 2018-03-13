b03c30097cb79be0f5936e9bd1238474ea3f3115
Phuong Lam <phuong@circe.xyz>
2018-03-13 09:59:55 +0700

    Remove slack notify

--- src/main/scala/circe/coin/util/Cronning.scala
+++ src/main/scala/circe/coin/util/Cronning.scala
@@ -1,11 +1,12 @@
 package circe.coin.util
 
+import com.twitter.inject.Logging
 import com.twitter.util.NonFatal
 
 /**
  * Created by phg on 12/27/17.
  **/
-trait Cronning extends XLogging {
+trait Cronning extends Logging {
   var threads = Seq[Thread]()
   def run(period: Long)(fn: => Unit): Unit = {
     val thread = new Thread(new Runnable() {
@@ -13,7 +14,7 @@ trait Cronning extends XLogging {
         fn
         Thread.sleep(period)
       } catch {
-        case NonFatal(throwable) => exception(throwable, "Cronning.run")
+        case NonFatal(throwable) => error("Cronning.run", throwable)
       }
     })
     thread.start()
