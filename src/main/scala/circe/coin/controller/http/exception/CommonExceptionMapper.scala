ed07a6a7dba037ea52dc8e4b0d17b99019254ff4
Phuong Lam <phuong@circe.xyz>
2018-03-13 10:21:52 +0700

    Update dev config

--- src/main/scala/circe/coin/controller/http/exception/CommonExceptionMapper.scala
+++ src/main/scala/circe/coin/controller/http/exception/CommonExceptionMapper.scala
@@ -6,15 +6,16 @@ import circe.coin.domain.StandardResponse
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.finatra.http.exceptions.ExceptionMapper
 import com.twitter.finatra.http.response.ResponseBuilder
+import com.twitter.inject.Logging
 
 /**
  * Created by plt on 11/16/17.
  **/
-class CommonExceptionMapper @Inject()(response: ResponseBuilder) extends ExceptionMapper[Exception] {
+class CommonExceptionMapper @Inject()(response: ResponseBuilder) extends ExceptionMapper[Exception] with Logging {
   override def toResponse(request: Request, throwable: Exception): Response = {
     throwable match {
       case s@_ =>
-        throwable.printStackTrace()
+        error("CommonExceptionMapper.toResponse", s)
         response.internalServerError(StandardResponse(code = -1, msg = s.getLocalizedMessage))
     }
   }
